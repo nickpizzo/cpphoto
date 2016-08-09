a148a04e771325ee35c51cb2cf335f0572bb4c31
Nick Pizzo <nick.pizzo@gmail.com>
2016-08-09 00:41:34 -0700

    navigation concept

--- src/Nav.js
+++ src/Nav.js
@@ -5,9 +5,13 @@ var Nav = React.createClass({
   render: function () {
     return(
       <div>
-        <p><IndexLink to="/" activeClassName="active-link">Pictures</IndexLink></p>
-        <p><Link to="/about" activeClassName="active-link">About</Link></p>
-        <p><Link to="/contact" activeClassName="active-link">Contact</Link></p>
+        <header className="navigation">
+          <p><IndexLink to="/" activeClassName="active-link">Pictures</IndexLink></p>
+          <ul>
+            <h4><Link to="/about" className="link" activeClassName="active-link">About</Link></h4>
+            <h4><Link to="/contact" className="link" activeClassName="active-link">Contact</Link></h4>
+          </ul>
+        </header>
       </div>
     )
   }
