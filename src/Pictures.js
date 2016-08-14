46847d12cd903a8938bc41a8cc535a8c188ddcd9
Nick Pizzo <nick.pizzo@gmail.com>
2016-08-14 14:08:47 -0700

    photo grid working

--- src/Pictures.js
+++ src/Pictures.js
@@ -1,13 +1,46 @@
 import React, { Component } from 'react';
+import Gallery from 'react-photo-gallery';
 
 class Pictures extends Component {
   render () {
     return(
-      <div>
-        <p>Pictures page</p>
-      </div>
+      <Gallery photos={PHOTO_SET} />
     )
   }
 };
+
+const PHOTO_SET = [
+  {
+    src: 'http://placehold.it/350x150',
+    width: 681,
+    height: 1024,
+    aspectRatio: 1.5,
+    lightboxImage:{
+    src: 'http://placehold.it/350x150',
+    srcset: [
+      'http://placehold.it/350x150 1024w',
+      'http://placehold.it/350x150 800w',
+      'http://placehold.it/350x150 500w',
+      'http://placehold.it/350x150 320w',
+    ]
+    }
+  },
+  {
+    src: 'http://placehold.it/350x150',
+    width: 600,
+    height: 600,
+    aspectRatio: 1,
+    lightboxImage:{
+    src: 'http://placehold.it/350x150',
+    srcset: [
+      'http://placehold.it/350x150 1024w',
+      'http://placehold.it/350x150 800w',
+      'http://placehold.it/350x150 500w',
+      'http://placehold.it/350x150 320w',
+    ]
+    }
+  }
+];
+
 
 export default Pictures;
